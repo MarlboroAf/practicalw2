0cedcfc6be8b3821489d7ea9ebe49a8715ed8b84
XIAOFAN CHEN <x.chen2.23@abdn.ac.uk>
2024-03-12 15:51:05 +0000

    temperature

--- temp_stories/views.py
+++ temp_stories/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    mystory = story()
+    converted_t = None
+    temp = None
+    if request.method== "POST":
+        temp = int(request.POST.get('temp',''))
+        converted_t = (temp-32)*0.5556
+    return render(request, 'temp_stories/index.html', {'story': mystory, 'converted_t': converted_t, 'temp': temp})
